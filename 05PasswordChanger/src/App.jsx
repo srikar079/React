6b7fc8648dcdcf17e6fa834127a2f958c59f154a
srikar079 <srikar079@gmail.com>
2024-08-15 17:21:26 +0530

    after finishing project 5

--- 05PasswordChanger/src/App.jsx
+++ 05PasswordChanger/src/App.jsx
@@ -1,13 +1,46 @@
-import { useState } from 'react'
+  import { useCallback, useState,useEffect } from 'react'
 
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+  const [password,setPassword]=useState("");
+  const[length,setlength]=useState(8);
+  const[letters,setLetters]= useState(true);
+  const[numbers,setNumbers]= useState(true);
+  const yesnol = () => {
+    setLetters(prevLetters => !prevLetters);  // Toggle letters state
+  };
+  const passwordgenerator=useCallback(()=>{
+    var pass="";
+    var x='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if(letters){
+      x+='12345678890';
+    }
+    if(numbers){
+    x+='+-*/)(_=';
+    }
+    for(var i =0;i<length;i++){
+      pass+=x.charAt((Math.floor) (Math.random()*length+1));
+    }
+    console.log('usecall')
+    setPassword(pass);
+
+  },[length,letters,numbers])
+  useEffect(()=>{
+    console.log('useef')
+    passwordgenerator();
+  },[length,letters,numbers,passwordgenerator])
   return (
     <>
-     <h1>Password generator</h1>  
+    <div style={{backgroundColor:'blue', height:300,width:400, margin:`40px 100px 120px 80px`}}>
+      <div>
+      <input type='textbox' name="ap" value={password} readOnly></input>
+      </div>
+    <button id="ok" onClick={() => yesnol()}>Letters</button>
+    <button id="ok" onClick={() => yesnon()}>Numbers</button>
+    
+    </div>
     </>
   )
 }
